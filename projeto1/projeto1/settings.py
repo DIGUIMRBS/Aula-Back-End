3289bc0f3606794bc7c75d0a85860d461d97060f
Rodrigo Bueno <rodrigobueno@gmail.com>
2024-08-27 21:55:59 -0300

    Atualização Aula 4

--- projeto1/projeto1/settings.py
+++ projeto1/projeto1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todos.apps.TodosConfig'
+    'todos.apps.TodosConfig',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt=br'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
--- projeto1/todos/views.py
+++ projeto1/todos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+#create yous views here
+
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'todos/home.html');
+
+
 # Create your views here.
